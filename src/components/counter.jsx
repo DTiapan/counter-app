ab8802132d75db0e8079afa588d51f12983ab7a8
DTipan <ajas.bakran@gmail.com>
2019-01-13 15:06:14 +0530

    completed app with local state

---
+++ src/components/counter.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+  state = {
+    count: this.props.counter.value
+  };
+
+  handleIncrement = () => {
+    console.log("raised");
+    this.setState({ count: this.state.count + 1 });
+  };
+  render() {
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secodary btn-sm"
+        >
+          Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger m-2"
+        >
+          Delete
+        </button>
+      </div>
+    );
+  }
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
+}
+
+export default Counter;
